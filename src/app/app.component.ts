8275999e2a48a07742a059d58b7083dde22ef0f1
sebastian <bastip@live.de>
2025-03-21 19:52:14 +0100

    working recording

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  signal,
+} from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+
+let recognition: SpeechRecognition;
+if ('webkitSpeechRecognition' in window) {
+  recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.lang = window.navigator.language;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,4 +20,41 @@ import { MatIcon } from '@angular/material/icon';
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  isRecording = signal(false);
+  text = signal('');
+  isListening = signal(false);
+
+  constructor() {
+    effect(() => {
+      if (this.text()) {
+        alert(this.text());
+      }
+    });
+
+    effect(() => {
+      const isRecording = this.isRecording();
+
+      if (isRecording) {
+        this.text.set('');
+        this.isListening.set(false);
+        recognition.start();
+        return;
+      }
+
+      recognition.onresult = (evt: SpeechRecognitionEvent) => {
+        this.text.set(evt.results[0][0].transcript);
+        this.isListening.set(true);
+        recognition.stop();
+      };
+    });
+  }
+
+  startRecording() {
+    this.isRecording.set(true);
+  }
+
+  stopRecording() {
+    this.isRecording.set(false);
+  }
+}
