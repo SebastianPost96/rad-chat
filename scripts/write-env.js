307e4789509abe7f1d1aa5b0990c32af6c692b18
sebastian <bastip@live.de>
2025-03-22 17:21:35 +0100

    js script

---
+++ scripts/write-env.js
@@ -0,0 +1,10 @@
+import { writeFile } from "fs";
+
+const targetPath = "./env.ts";
+const envConfigFile = process.env.ENVIRONMENT;
+
+writeFile(targetPath, envConfigFile, "utf8", (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
